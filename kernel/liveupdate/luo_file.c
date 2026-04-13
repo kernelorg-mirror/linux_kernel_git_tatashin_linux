4f36bf755c349041e4fc613e54a9c0e5e6ed360c
Pasha Tatashin <pasha.tatashin@soleen.com>
2026-04-13 15:59:16 +0000

    Revert "liveupdate: defer file handler module refcounting to active sessions"
    This reverts commit 37b5be9ab3011b3d474322979a7e81fb24c26fed.

--- kernel/liveupdate/luo_file.c
+++ kernel/liveupdate/luo_file.c
@@ -291,8 +291,7 @@ int luo_preserve_file(struct luo_file_set *file_set, u64 token, int fd)
 	down_read(&luo_register_rwlock);
 	list_private_for_each_entry(fh, &luo_file_handler_list, list) {
 		if (fh->ops->can_preserve(fh, file)) {
-			if (try_module_get(fh->ops->owner))
-				err = 0;
+			err = 0;
 			break;
 		}
 	}
@@ -305,7 +304,7 @@ int luo_preserve_file(struct luo_file_set *file_set, u64 token, int fd)
 	err = xa_insert(&luo_preserved_files, luo_get_id(fh, file),
 			file, GFP_KERNEL);
 	if (err)
-		goto err_module_put;
+		goto err_free_files_mem;
 
 	err = luo_flb_file_preserve(fh);
 	if (err)
@@ -341,8 +340,6 @@ err_flb_unpreserve:
 	luo_flb_file_unpreserve(fh);
 err_erase_xa:
 	xa_erase(&luo_preserved_files, luo_get_id(fh, file));
-err_module_put:
-	module_put(fh->ops->owner);
 err_free_files_mem:
 	luo_free_files_mem(file_set);
 err_fput:
@@ -385,7 +382,6 @@ void luo_file_unpreserve_files(struct luo_file_set *file_set)
 		args.private_data = luo_file->private_data;
 		luo_file->fh->ops->unpreserve(&args);
 		luo_flb_file_unpreserve(luo_file->fh);
-		module_put(luo_file->fh->ops->owner);
 
 		xa_erase(&luo_preserved_files,
 			 luo_get_id(luo_file->fh, luo_file->file));
@@ -677,7 +673,6 @@ static void luo_file_finish_one(struct luo_file_set *file_set,
 
 	luo_file->fh->ops->finish(&args);
 	luo_flb_file_finish(luo_file->fh);
-	module_put(luo_file->fh->ops->owner);
 }
 
 /**
@@ -815,8 +810,7 @@ int luo_file_deserialize(struct luo_file_set *file_set,
 		down_read(&luo_register_rwlock);
 		list_private_for_each_entry(fh, &luo_file_handler_list, list) {
 			if (!strcmp(fh->compatible, file_ser[i].compatible)) {
-				if (try_module_get(fh->ops->owner))
-					handler_found = true;
+				handler_found = true;
 				break;
 			}
 		}
@@ -830,10 +824,8 @@ int luo_file_deserialize(struct luo_file_set *file_set,
 		}
 
 		luo_file = kzalloc_obj(*luo_file);
-		if (!luo_file) {
-			module_put(fh->ops->owner);
+		if (!luo_file)
 			return -ENOMEM;
-		}
 
 		luo_file->fh = fh;
 		luo_file->file = NULL;
@@ -894,6 +886,12 @@ int liveupdate_register_file_handler(struct liveupdate_file_handler *fh)
 		}
 	}
 
+	/* Pin the module implementing the handler */
+	if (!try_module_get(fh->ops->owner)) {
+		err = -EAGAIN;
+		goto err_unlock;
+	}
+
 	INIT_LIST_HEAD(&ACCESS_PRIVATE(fh, flb_list));
 	INIT_LIST_HEAD(&ACCESS_PRIVATE(fh, list));
 	list_add_tail(&ACCESS_PRIVATE(fh, list), &luo_file_handler_list);
@@ -923,4 +921,6 @@ void liveupdate_unregister_file_handler(struct liveupdate_file_handler *fh)
 	guard(rwsem_write)(&luo_register_rwlock);
 	luo_flb_unregister_all(fh);
 	list_del(&ACCESS_PRIVATE(fh, list));
+
+	module_put(fh->ops->owner);
 }
